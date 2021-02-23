82d5c372ecea40084e46b831afce678a1d6d56eb
Juliette McCavana <jmccavana@aol.com>
2021-02-23 13:12:44 +0100

    ajout axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 import router from './router'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import axios from 'axios';
+
+axios.defaults.baseURL = "http://localhost:3000/api/";
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router,
   vuetify,
   render: h => h(App)
